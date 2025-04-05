1660cbbe5a538c84261cb770f6124c66939a0297
Parag Chauhan <iamparag121298@gmail.com>
2025-04-05 18:36:06 +0530

    metadata changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,26 @@ import Header from "@/components/header/Header";
 export const metadata = {
   title: "Parag Chauhan",
   description: "A portfolio by and of Parag Chauhan",
+  openGraph: {
+    title: "Parag Chauhan - Portfolio",
+    description: "Explore the portfolio of Parag Chauhan, showcasing unique digital experiences.",
+    // url: "https://yourwebsite.com",
+    type: "website",
+    images: [
+      {
+        url: "/images/parag.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Parag Chauhan Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Parag Chauhan - Portfolio",
+    description: "Explore the portfolio of Parag Chauhan, showcasing unique digital experiences.",
+    image: "/images/sprout.webp",
+  },
 };
 
 export default function RootLayout({ children }) {
